5f58c3fe5125ea5902d3483e638b1b81c914b08f
Dell <Dell@DESKTOP-97HN4GD>
2022-06-09 17:24:02 -0600

    Agregando entity framework code first

--- PANMI/PANMI.BL/ProductosBL.cs
+++ PANMI/PANMI.BL/ProductosBL.cs
@@ -8,33 +8,21 @@ namespace PANMI.BL
 {
     public class ProductosBL
     {
-  
+        Contexto _contexto;
+        public List<Producto> ListadeProductos { get; set; }
+
+
+        public ProductosBL()
+        {
+            _contexto = new Contexto();
+            ListadeProductos = new List<Producto>();
+     }
+
         public List<Producto> ObtenerProductos()
         {
-            var producto1 = new Producto();
-            producto1.Id = 1;
-            producto1.Descripcion = "Bolsa de Pan Blanco";
-            producto1.Precio = 65;
+            ListadeProductos = _contexto.Productos.ToList();
+            return ListadeProductos;
 
-            var producto2 = new Producto();
-            producto2.Id = 2;
-            producto2.Descripcion = "Bolsa de Pan Molde";
-            producto2.Precio = 45;
-
-
-            var producto3 = new Producto();
-            producto3.Id = 3;
-            producto3.Descripcion = "Bolsa de Pan Hamburguesa";
-            producto3.Precio = 80;
-
-
-
-            var listadeProductos = new List<Producto>();
-            listadeProductos.Add(producto1);
-            listadeProductos.Add(producto2);
-            listadeProductos.Add(producto3);
-
-            return listadeProductos; 
-        } 
+        }
     }
 }
